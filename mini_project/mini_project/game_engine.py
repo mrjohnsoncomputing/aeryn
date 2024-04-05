339fdd089b3203dc53327da9a2819a5e1941ef6d
MrJohnsonComputing <accounts@mrjohnsoncomputing.co.uk>
2024-04-05 02:08:05 +0100

    Added original code, removed blarg, add type hints

--- mini_project/mini_project/game_engine.py
+++ mini_project/mini_project/game_engine.py
@@ -1,5 +1,5 @@
 from pygame import init, display, time, image, event
-from pygame import Surface
+from pygame import Surface, Rect
 from pygame import USEREVENT, QUIT
 from pygame.transform import scale
 from pygame.font import SysFont
@@ -11,28 +11,23 @@ from .player import Player
 
 class GameEngine:
     def __init__(self, screen_width: int, screen_height: int) -> None:
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+        self.screen_width: int = screen_width
+        self.screen_height: int = screen_height
 
         self.screen: Surface = None
         self.target_image: Surface = None
         
         self.is_running: bool = False
-        self.counter = 0
-        self.text = str(self.counter)
+        self.counter: int = 0
 
-        self.max_targets = 5
+        self.max_targets: int = 5
 
-    def setup(self):
+    def setup(self) -> None:
         init()
         self.screen = display.set_mode((self.screen_width, self.screen_height))
         # Timer setup
         self.counter = 120
-        self.text = str(self.counter).rjust(3)
         time.set_timer(USEREVENT, 1000)
-
-    def check_collision(self, playerRect, targetRect):
-        return playerRect.colliderect(targetRect)
     
     def create_targets(self, count: int = 5) -> list[Target]:
         return [
@@ -49,7 +44,7 @@ class GameEngine:
             screen_width=self.screen_width, 
             image=self.target_image)
 
-    def display_score_and_timer(self, player: Player):
+    def display_score_and_timer(self, player: Player) -> None:
         # Display score and timer
         font = SysFont(None, 36)
         score_text = font.render('Score: {}'.format(player.score), True, "WHITE")
@@ -57,13 +52,13 @@ class GameEngine:
         self.screen.blit(score_text, (10, 10))
         self.screen.blit(timer_text, (10, 50))
 
-    def display_debug_info(self, entity: Player | Target, offset: int = 0):
-        font = SysFont(None, 36)
+    def display_debug_info(self, entity: Player | Target, offset: int = 0) -> None:
+        font = SysFont(None, 15)
         name = type(entity).__name__
-        score_text = font.render(f'{name} Coords: x: {entity.x}, y: {entity.y}', True, "WHITE")
-        timer_text = font.render(f'{name} Size: {entity.width}', True, "WHITE")
-        self.screen.blit(score_text, (10, 90 + offset))
-        self.screen.blit(timer_text, (10, 130 + offset))
+        coords = font.render(f'{name} Coords: x: {entity.x}, y: {entity.y}', True, "WHITE")
+        size = font.render(f'{name} Size: {entity.width}', True, "WHITE")
+        self.screen.blit(coords, (10, 90 + offset))
+        self.screen.blit(size, (10, 100 + offset))
 
 
     def get_surviving_targets(self, targets: list[Target]) -> list[Target]:
@@ -75,12 +70,13 @@ class GameEngine:
 
     def add_new_target(self, player: Player) -> Target:
         # Add a new target making sure it does not collide with the player immediately
-        while True:
+        colliding = True
+        while colliding:
             new_target = self.create_target()
-            if not player.is_colliding_with(new_target):
-                return new_target
+            colliding = player.is_colliding_with(new_target)
+        return new_target
 
-    def run(self, player: Player):
+    def run(self, player: Player) -> None:
         # Main game loop
         self.is_running = True
         targets = self.create_targets(count=self.max_targets) # Initialize/reinitialize targets here for fresh start on game reset
@@ -108,15 +104,19 @@ class GameEngine:
             player.move(self.screen)
             player.draw(self.screen)
 
+            offset_multiplier = 1 # This is just for debug - not for the actual gameplay
             for target in targets:
                 target.move(screen_height=self.screen_height, screen_width=self.screen_width)
                 target.draw(screen=self.screen)
                 
+
+                self.display_debug_info(entity=target, offset=30 * offset_multiplier)
+                offset_multiplier += 1
+
                 if player.is_colliding_with(target):
                     if player.is_larger_than(target):
                         player.grow()
                         target.get_eaten()
-
             
             targets = self.get_surviving_targets(targets=targets)
             
@@ -127,8 +127,7 @@ class GameEngine:
                     targets.append(self.add_new_target(player=player))
 
             self.display_score_and_timer(player=player)
-            # self.display_debug_info(entity=player)
-            # self.display_debug_info(entity=target, offset=80)
-
+            self.display_debug_info(entity=player)
+            
             display.flip()
             clock.tick(60)
